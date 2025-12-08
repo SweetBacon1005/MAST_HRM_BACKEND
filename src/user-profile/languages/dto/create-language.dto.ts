8a2286e594fc1a596fd033bd8a5a631de4ed2654
SweetBacon1005 <nguyentrungsb1005@gmail.com>
2025-12-08 22:05:01 +0700

    fix

--- src/user-profile/languages/dto/create-language.dto.ts
+++ src/user-profile/languages/dto/create-language.dto.ts
@@ -11,25 +11,4 @@ export class CreateLanguageDto {
   @IsNotEmpty()
   @MaxLength(255)
   name: string;
-
-  @ApiProperty({
-    description: 'Mã ngôn ngữ (ISO 639-1)',
-    example: 'en',
-    maxLength: 10,
-  })
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(10)
-  code: string;
-
-  @ApiProperty({
-    description: 'Mô tả ngôn ngữ',
-    example: 'Tiếng Anh',
-    required: false,
-    maxLength: 500,
-  })
-  @IsOptional()
-  @IsString()
-  @MaxLength(500)
-  description?: string;
 }
